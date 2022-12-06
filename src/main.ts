f8dc9301a4578a36269251dd414c0d176ecf0570
Chrystian Silva <chrystian100@hotmail.com>
2022-12-06 08:37:15 -0300

    Adjusted main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
 }
 bootstrap();
