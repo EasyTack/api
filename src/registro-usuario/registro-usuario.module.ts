f5fb2ea9d1ad195c93dff617c4ff2a37b4696bb0
Chrystian Silva <chrystian100@hotmail.com>
2022-12-07 22:20:28 -0300

    Adjusted registro-usuario module

--- src/registro-usuario/registro-usuario.module.ts
+++ src/registro-usuario/registro-usuario.module.ts
@@ -4,10 +4,11 @@ import { RegistroUsuarioController } from './registro-usuario.controller';
 import { UsuariosService } from '../usuarios/usuarios.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Usuario } from '../usuarios/entities/usuario.entity';
+import { ConfiguracoesService } from 'src/configuracoes/configuracoes.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Usuario])],
   controllers: [RegistroUsuarioController],
-  providers: [RegistroUsuarioService,UsuariosService]
+  providers: [RegistroUsuarioService, UsuariosService, ConfiguracoesService]
 })
-export class RegistroUsuarioModule {}
+export class RegistroUsuarioModule { }
